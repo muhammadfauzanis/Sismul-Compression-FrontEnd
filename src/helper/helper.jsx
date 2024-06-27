286c86e97b04a16c4c7836aaedde1861a7c8aae3
faisalach <faisalachramsyah@gmail.com>
2024-06-27 13:27:35 +0700

    update test

--- src/helper/helper.jsx
+++ src/helper/helper.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom"; */
 export const Helper = () => {
     const baseURLAPI = (url = '') => {
         url     = url.replace(/^[/]/g,'');
-        // const baseURL   = 'https://kompresi-sismul-orta27ik3q-et.a.run.app/api/';
-        const baseURL   = 'http://127.0.0.1:5000/api/';
+        const baseURL   = 'https://kompresi-sismul-orta27ik3q-et.a.run.app/api/';
+        // const baseURL   = 'http://127.0.0.1:5000/api/';
         return baseURL + url;
     }
 
